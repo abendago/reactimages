e074102ea5bc4b0de25ef6731739b0c87ef551d7
Nathan Leggatt <nathan@vanarts.com>
2020-11-14 10:23:39 -0800

    initial setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './css/style.css'
+import bg from './static/assets/images/bg.png'
+function App() {
+    return (
+      <h1 style={{backgroundImage: "url(" + bg + ")"}}>In THe App Here</h1>
+    );
+  }
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+};
